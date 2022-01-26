64713d642fd2188d2da23c0ca21ccdab0b522edd
Rimaansh <67331016+Rimaansh@users.noreply.github.com>
2022-01-26 19:53:02 +0530

    Changes in RemoveParticularChar

--- Recursion/RemoveParticularChar.java
+++ Recursion/RemoveParticularChar.java
@@ -1,29 +1,28 @@
 package com.dsapackage.Recursion;
 
-import java.sql.SQLOutput;
-
 public class RemoveParticularChar {
     public static void main(String[] args)
     {
 
     }
 
-    static String removeChar(String str, char ch, String nstr, int pointer)
-    {
+    static String removeChar(String str, char ch, String nstr, int pointer) {
         if(pointer==str.length()) return nstr;
 
         if(str.charAt(pointer) != ch) nstr = nstr + str.charAt(pointer);
         return removeChar(str, ch, nstr, pointer+1);
     }
 
-    static String removeChar(String str, char ch, int pointer)
-    {
-        if(pointer==str.length() || str.length()==0) return "";
+    static String removeChar(String str, char ch, int pointer){
+        String temp = "";
 
-        String s="";
-        if(str.charAt(pointer)!=ch)  s = s + str.charAt(pointer) + removeChar(str, ch, pointer + 1);
-        else s = s + removeChar(str, ch, pointer + 1);
+        if(pointer == str.length()) return "";
 
-        return s;
+        if(str.charAt(pointer) != ch){
+            temp = temp + str.charAt(pointer);
+        }
+
+        temp = temp + removeChar(str, ch, pointer +1);
+        return temp;
     }
 }
