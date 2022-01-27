a46091d744d6d23da2b259d841683a4202d56c3d
Rimaansh <67331016+Rimaansh@users.noreply.github.com>
2022-01-27 11:55:37 +0530

    Added FibonacciNum509

---
+++ Leetcode/FibonacciNum509.java
@@ -0,0 +1,13 @@
+package com.dsapackage.Leetcode;
+
+public class FibonacciNum509
+{
+    public static void main(String[] args) {
+        System.out.println(fib(4));
+    }
+
+    static int fib(int n) {
+        if(n == 0 || n ==1) return n;
+        return fib(n-1) + fib(n-2);
+    }
+}
