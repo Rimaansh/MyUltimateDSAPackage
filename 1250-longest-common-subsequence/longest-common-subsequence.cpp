7b72de873abdc573bbf0e0e043b16392b0eb3e46
Rimaansh Bhargava <67331016+Rimaansh@users.noreply.github.com>
2024-01-16 19:23:26 +0530

    Time: 45 ms (69.17%) | Memory: 24.8 MB (37.57%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -18,7 +18,29 @@ public:
         n1 = text1.length();
         n2 = text2.length();
 
-        vector<vector<int>> dp(n1, vector<int>(n2, -1));
-        return solve(text1, text2, n1 - 1, n2 - 1, dp);
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1, 0));
+
+        //return solve(text1, text2, n1 - 1, n2 - 1, dp);
+
+        string s1 = text1;
+        string s2 = text2;
+
+        for(int i = 1; i<=n1; i++)
+        {
+            for(int j = 1; j<=n2; j++)
+            {
+                if(s1[i - 1] == s2[j - 1])
+                {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else
+                {
+                    dp[i][j] = 0 + max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+
+        return dp[n1][n2];
+
     }
 };
