3ea0c9b938a7fe9f0255c4e94e440ea5d51705ee
Rimaansh Bhargava <67331016+Rimaansh@users.noreply.github.com>
2024-01-19 19:46:37 +0530

    Time: 19 ms (92.04%) | Memory: 31.4 MB (18.85%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -17,8 +17,33 @@ public:
     }
 
     int numDistinct(string s, string t) {
-        vector<vector<int>> dp(s.length(), vector<int>(t.length(), -1));
+        int n = s.length();
+        int m = t.length();
+        vector<vector<long long>> dp(n+1, vector<long long>(m+1, 0));
 
-        return solve(s, t, s.length()-1, t.length()-1, dp);
+        //return solve(s, t, s.length()-1, t.length()-1, dp);
+        for(int i = 0; i<=n; i++){
+            dp[i][0] = 1;
+        }
+
+        for(int j = 1; j<=m; j++){
+            dp[0][j] = 0;
+        }
+
+        for(int i = 1; i<=n; i++)
+        {
+            for(int j = 1; j<=m; j++)
+            {
+                long long pick = 0, notPick;
+
+                if(s[i-1] == t[j-1]) pick = dp[i-1][j-1];
+
+                notPick = dp[i-1][j];
+
+                dp[i][j] = (int)pick + notPick;
+            }
+        }
+
+        return dp[n][m];
     }
 };
