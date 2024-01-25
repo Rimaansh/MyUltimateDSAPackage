4a9d3a9570ecbeb160e36a947e86897d14a83402
Rimaansh Bhargava <67331016+Rimaansh@users.noreply.github.com>
2024-01-25 21:21:31 +0530

    Time: 22 ms (88.88%) | Memory: 25.7 MB (6.35%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    int solve(string &s1, string &s2, int ind1, int ind2, vector<vector<int>> &dp)
-    {
-        if(ind1 < 0 || ind2 < 0) return 0;
+    int solve(int n, int m, string &s1, string &s2, vector<vector<int>> &dp)
+{
+	if(n < 0 || m < 0) return 0;
 
-        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
+	if(dp[n][m] != -1) return dp[n][m];
 
-        if(s1[ind1] == s2[ind2]) return dp[ind1][ind2] = 1 + solve(s1, s2, ind1 - 1, ind2 - 1, dp);
-        else
-        {
-            return dp[ind1][ind2] = max(solve(s1, s2, ind1 - 1, ind2, dp), solve(s1, s2, ind1, ind2 - 1, dp));
-        }
-    }
+	if(s1[n] == s2[m]) 
+	{
+		return dp[n][m] = 1 + solve(n-1, m-1, s1, s2, dp);
+	}
+
+	return dp[n][m] = max(solve(n, m-1, s1, s2, dp), solve(n-1, m, s1, s2, dp));
+}
 
     int longestCommonSubsequence(string text1, string text2) {
         int n1, n2;
