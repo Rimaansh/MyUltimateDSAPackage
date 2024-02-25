1f5ce3c360704078528d8eaff628e81fd6ea2f9d
Rimaansh Bhargava <67331016+Rimaansh@users.noreply.github.com>
2024-02-26 04:11:59 +0530

    Time: 115 ms (68.95%) | Memory: 68.9 MB (62.13%) - LeetSync

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool check(vector<int>& bloomDay, int m, int k, int mid)
+    {
+        int c = 0;
+        for(auto it : bloomDay){
+            if(it <= mid){
+                c++;
+                if(c == k){
+                    m--;
+                    c = 0;
+                }
+            }
+            else c = 0;
+        }
+        return m <= 0;
+    }
+
+    int minDays(vector<int>& bloomDay, int m, int k) 
+    {
+        int n = bloomDay.size();
+        if(m > n/k) return -1;
+
+        int lo = *min_element(bloomDay.begin(), bloomDay.end());
+        int hi = *max_element(bloomDay.begin(), bloomDay.end());
+        int ans = hi;
+
+        while(lo <= hi)
+        {
+            int mid = lo + (hi-lo)/2;
+
+            if(check(bloomDay, m, k, mid))
+            {
+                ans = min(ans, mid);
+                hi = mid - 1;
+            }
+            else lo = mid + 1;
+        }
+
+        return ans;
+    }
+};
