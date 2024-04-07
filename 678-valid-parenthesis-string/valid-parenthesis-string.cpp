a5e7268888d6c03f199886384bbc784888358d9f
Rimaansh Bhargava <67331016+Rimaansh@users.noreply.github.com>
2024-04-07 14:39:43 +0530

    Time: 3 ms (45.50%) | Memory: 7.5 MB (39.07%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        int leftMin = 0, leftMax = 0;
-
-        for (char c : s) {
-            if (c == '(') {
-                leftMin++;
-                leftMax++;
-            } else if (c == ')') {
-                leftMin--;
-                leftMax--;
-            } else {
-                leftMin--;
-                leftMax++;
+        stack<int> asterisk;
+        stack<int> validPar;
+        
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == '(') validPar.push(i);
+            if(s[i] == '*') asterisk.push(i);
+            if(s[i] == ')'){
+                if(!validPar.empty()) 
+                    validPar.pop();
+                else if(!asterisk.empty()) 
+                    asterisk.pop();
+                else 
+                    return false;
             }
-            if (leftMax < 0) return false;
-            if (leftMin < 0) leftMin = 0;
         }
         
-        return leftMin == 0;
+        while(!validPar.empty() && !asterisk.empty())
+        {
+            if(validPar.top() > asterisk.top()) return false;
+            validPar.pop(); 
+            asterisk.pop();
+        }
+        
+        return validPar.empty();
     }
 };
