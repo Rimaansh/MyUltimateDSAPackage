c0d01b10be8ebb8372238e87399e88ac52e68ab7
Rimaansh Bhargava <67331016+Rimaansh@users.noreply.github.com>
2024-09-24 20:39:12 +0530

    Time: 1086 ms (6.44%) | Memory: 362.1 MB (8.72%) - LeetSync

---
+++ 3541-report-spam-message/report-spam-message.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool reportSpam(vector<string>& message, vector<string>& bannedWords) {
+        map<string, int> mpp;
+        for(auto it: message) mpp[it]++;
+
+        int c = 0;
+        for(auto it: bannedWords) if(mpp[it] > 0) {
+            c+=mpp[it];
+            mpp[it] = -1;
+        }
+
+        return c>=2;
+    }
+};
