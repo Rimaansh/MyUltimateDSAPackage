0a75ef446d78a59e1a520f21789b086b5706c165
Rimaansh Bhargava <67331016+Rimaansh@users.noreply.github.com>
2025-11-17 17:41:06 +0000

    Time: 0 ms (100.00%) | Memory: 11 MB (31.98%) - LeetSync

---
+++ 93-restore-ip-addresses/restore-ip-addresses.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<string> restoreIpAddresses(string s) 
+    {
+        vector<string> res;
+        for(int i = 1; i<=3; i++)
+        {
+            for(int j = 1; j<=3; j++)
+            {
+                for(int k = 1; k<=3; k++)
+                {
+                    for(int l = 1; l<=3; l++)
+                    {
+                        if(i+j+k+l == s.length())
+                        {
+                            int a = stoi(s.substr(0, i));
+                            int b = stoi(s.substr(i, j));
+                            int c = stoi(s.substr(i+j, k));
+                            int d = stoi(s.substr(i+j+k, l));
+                            if(a <= 255 && b <= 255 && 
+                                c <= 255 && d <= 255)
+                            {
+                                string ans = to_string(a) + "." + 
+                                to_string(b) + "." + to_string(c) 
+                                + "." + to_string(d);
+                                
+                                if(ans.length() == s.length() + 3)
+                                    res.push_back(ans);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+};
