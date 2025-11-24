d1731dcad94bd3722647c783f23c32319898fa17
Rimaansh Bhargava <67331016+Rimaansh@users.noreply.github.com>
2025-11-24 17:16:58 +0000

    Time: 3 ms (53.70%) | Memory: 14.7 MB (34.82%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,34 +1,39 @@
 class Solution {
 public:
-    void solve(vector<int>& nums, int target, int ind, vector<int>& combi, vector<vector<int>>& res)
+    void func(vector<int>& candidates, int target, int i, vector<int>& nums, vector<vector<int>>& res, map<vector<int>, int>& mpp)
     {
         if(target == 0){
-            res.push_back(combi);
+            if(mpp.find(nums) == mpp.end()){
+                res.push_back(nums);
+                mpp[nums]++;
+                return;
+            }
+            else return;
+        }
+
+        if(i == candidates.size() || target < 0){
             return;
         }
-        
-        if(ind < 0) return;
 
-        //pick the number if it lest than or equal to the target right now
-        if(nums[ind] <= target)
-        {
-            combi.push_back(nums[ind]);
-            solve(nums, target - nums[ind], ind, combi, res); //not decrementing the index as 1 number can be chosen unlimited times
-            combi.pop_back();
+        // Pick has two cases, take the same number again or go to the next
+        if(target - candidates[i] >= 0){
+            nums.push_back(candidates[i]);
+            func(candidates, target - candidates[i], i, nums, res, mpp);
+            func(candidates, target - candidates[i], i+1, nums, res, mpp);
+            nums.pop_back();
         }
 
-        //not pick the number
-        solve(nums, target, ind-1, combi, res);
-
-        return;
+        //Not picking the current number
+        func(candidates, target, i+1, nums, res, mpp);
     }
 
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) 
+    {
+        vector<int> nums;
         vector<vector<int>> res;
-        vector<int> combi;
+        map<vector<int>, int> mpp;
 
-        solve(candidates, target, candidates.size()-1, combi, res);
-
+        func(candidates, target, 0, nums, res, mpp);
         return res;
     }
 };
