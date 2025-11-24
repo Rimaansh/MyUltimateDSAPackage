b9cf39afba28f69e5a2f9a214a838905fc9d4819
Rimaansh Bhargava <67331016+Rimaansh@users.noreply.github.com>
2025-11-24 17:19:44 +0000

    Time: 0 ms (100.00%) | Memory: 14.1 MB (72.56%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
-    void func(vector<int>& candidates, int target, int i, vector<int>& nums, vector<vector<int>>& res, map<vector<int>, int>& mpp)
+    void func(vector<int>& candidates, int target, int i, vector<int>& nums, vector<vector<int>>& res)
     {
         if(target == 0){
-            if(mpp.find(nums) == mpp.end()){
-                res.push_back(nums);
-                mpp[nums]++;
-                return;
-            }
-            else return;
+            res.push_back(nums);
+            return;
         }
 
         if(i == candidates.size() || target < 0){
             return;
         }
 
-        // Pick has two cases, take the same number again or go to the next
+        // Pick has two cases, take the same number again or go to the next so we will not
+        // do i+1
         if(target - candidates[i] >= 0){
             nums.push_back(candidates[i]);
-            func(candidates, target - candidates[i], i, nums, res, mpp);
-            func(candidates, target - candidates[i], i+1, nums, res, mpp);
+            func(candidates, target - candidates[i], i, nums, res);
             nums.pop_back();
         }
 
         //Not picking the current number
-        func(candidates, target, i+1, nums, res, mpp);
+        func(candidates, target, i+1, nums, res);
     }
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) 
     {
         vector<int> nums;
         vector<vector<int>> res;
-        map<vector<int>, int> mpp;
 
-        func(candidates, target, 0, nums, res, mpp);
+        func(candidates, target, 0, nums, res);
         return res;
     }
 };
