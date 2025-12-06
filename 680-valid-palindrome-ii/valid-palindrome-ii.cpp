cc871cebaa1145e1337783b665bd32be50047ae3
Rimaansh Bhargava <67331016+Rimaansh@users.noreply.github.com>
2025-12-06 22:49:10 +0000

    Time: 6 ms (39.37%) | Memory: 26.7 MB (32.21%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool helper(string s, int i, int j)
+    {
+        while(i<j)
+        {
+            if(s.at(i) == s.at(j)){
+                i++;
+                j--;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+        int i = 0, j = s.length() - 1;
+
+        while(i<j)
+        {
+            if(s.at(i) == s.at(j)){
+                i++;
+                j--;
+            }
+            else 
+            {
+                return helper(s, i+1, j) || helper(s, i, j-1);
+            }
+        }
+        
+        return true;
+    }
+};
