09c58694f924ec1e604d1756e72098758e603c91
Rimaansh Bhargava <67331016+Rimaansh@users.noreply.github.com>
2026-08-31 09:36:33 +0100

    Time: 272 ms (14.21%) | Memory: 34.1 MB (5.74%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,53 +1,32 @@
 class Solution(object):
-    def isValid(self, i, j, n, m):
-        if i < 0 or i >= n:
-            return False
-        if j < 0 or j >= m:
-            return False
-        
-        return True
-    
-    def bfs(self, i, j, vis, grid):
-        vis[i][j] = True
-        
-        q = deque()
-        
-        q.append((i, j))
-        
-        n = len(grid)
-        m = len(grid[0])
-
-        directions = [ (-1, 0), (0, 1), (1, 0), (0, -1) ]
-        
-        while q:
-            row, col = q.popleft()
-            
-            for delRow, delCol in directions:
-                newRow = row + delRow
-                newCol = col + delCol
-                
-                if (
-                    self.isValid(newRow, newCol, n, m) and 
-                    grid[newRow][newCol] == '1' and 
-                    not vis[newRow][newCol]
-                ):
-                    vis[newRow][newCol] = True
-                    
-                    q.append((newRow, newCol))
-    
     def numIslands(self, grid):
+        def isValid(r, c):
+            return r >= 0 and c >= 0 and r < n and c < m and grid[r][c] == '1'
+
+        def dfs(i, j):
+            if (i, j) in visited:
+                return 
+            
+            visited.add((i, j))
+
+            for dr, dc in directions:
+                r = i + dr
+                c = j + dc
+
+                if isValid(r, c) and (r, c) not in visited:
+                    dfs(r, c)
+
+        visited = set()
+
         n = len(grid)
         m = len(grid[0])
-        
-        vis = [[False for _ in range(m)] for _ in range(n)]
-        
-        count = 0
-        
+        islands = 0
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+
         for i in range(n):
             for j in range(m):
-                if not vis[i][j] and grid[i][j] == '1':
-                    count += 1
-                    self.bfs(i, j, vis, grid)
+                if grid[i][j] == '1' and (i, j) not in visited:
+                    dfs(i, j)
+                    islands += 1
         
-        return count
-        
+        return islands
