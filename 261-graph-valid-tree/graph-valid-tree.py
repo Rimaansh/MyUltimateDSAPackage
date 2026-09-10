c9b8e4db8ed80267cb940399ca4222921e24b628
Rimaansh Bhargava <67331016+Rimaansh@users.noreply.github.com>
2026-09-10 16:49:30 +0100

    Time: 0 ms (100.00%) | Memory: 20.5 MB (36.65%) - LeetSync

---
+++ 261-graph-valid-tree/graph-valid-tree.py
@@ -0,0 +1,25 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n - 1:
+            return False
+
+        graph = defaultdict(list)
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+
+        visited = set()
+        components = 0
+
+        def dfs(node):
+            visited.add(node)
+            for nei in graph[node]:
+                if nei not in visited:
+                    dfs(nei)
+
+        for node in range(n):
+            if node not in visited:
+                components += 1
+                dfs(node)
+
+        return components == 1
